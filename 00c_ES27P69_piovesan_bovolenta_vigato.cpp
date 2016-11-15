1cbb72beeedf405603a904d07af416961815f2ec
silviapioveasan <annarita.talucci@alice.it>
2016-11-15 19:34:30 +0100

    Add files via upload

---
+++ 00c_ES27P69_piovesan_bovolenta_vigato.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,d;
+    cout << "algoritmo che calcola i primi n numeri primi" << endl;
+    cout << "inserisci n >=1" << endl;
+    cin << n;
+    for(i=0; i<n; i++){
+
+    }
+    return 0;
+}
