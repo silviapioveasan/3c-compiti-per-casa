1c41b99ced3f86688bfd2bf4eba31d43673e4b69
silviapioveasan <annarita.talucci@alice.it>
2016-11-15 19:41:58 +0100

    Add files via upload

---
+++ 00c_ES4P68_piovesan_bovolenta_vigato.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+ int main()
+{ int X, Y, N , A;
+if(2 < N < 50)
+{ A= X+N*Y;
+cout << "il risultato è: " << A << endl;
+}
+else{
+cout << "il numero N deve essere compreso tra 2 e 50" << endl;
+}
+return 0;
+};
